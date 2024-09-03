cbb8c0738d76b26f8cb728f5bf4a1e8d7c7a588f
riktadhali <riktadhali031@gmail.com>
2024-09-03 10:55:13 +0600

    first commit

---
+++ Script.js
@@ -0,0 +1,30 @@
+var x=70;
+var y=20;
+var z=x+y;
+console.log(z);
+
+function myFunction(){
+   document.getElementById("demo").innerHTML='hi phd' ;
+   document.getElementById("Demo").style.fontSize = "35px";
+}
+
+function myFunction(name){
+    console.log(" hi" + name);
+}
+myFunction(" rikta");
+myFunction(" rikta");
+myFunction(" sumi");
+myFunction(" mamota");
+
+function myFunction(){
+document.getElementById('myImage').src='image/WhatsApp Image 2024-08-18 at 21.56.02_0f673bfd.jpg';
+document.getElementById('myImage').src='image/WhatsApp Image 2024-07-14 at 23.18.53_6dbd12a8.jpg';
+}
+
+function myFunction(){
+    document.getElementById('class').style.display="none";
+}
+
+    function myFunction(){
+   document.getElementById('Rikta').style.display='block';
+}
